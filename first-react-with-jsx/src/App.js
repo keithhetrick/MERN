9d4f09241571d2cac18ece76a52a2206d8b4f51b
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-07-11 19:11:13 -0500

    missing folders

---
+++ first-react-with-jsx/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 id='title'>Hello Dojo!</h1>
+      <h2 id='todoheader'>Things I need to do:</h2>
+      <ul id='todolist'>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}   
+
+export default App;
