24f8fec2a4f2aa4ca4157dea6ec135150f47441f
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-07-30 21:31:32 -0500

    postman pokemon assignment

---
+++ postman-pokemon/App.js
@@ -0,0 +1,166 @@
+Postman + Pokemon Assignment
+
+
+***ALL RESPONSES ARE SHORTENED DUE TO LENGTH***
+
+
+Request 1 - Bulbasaur: https://pokeapi.co/api/v2/pokemon/bulbasaur
+
+{
+  "abilities": [
+      {
+          "ability": {
+              "name": "overgrow",
+              "url": "https://pokeapi.co/api/v2/ability/65/"
+          },
+          "is_hidden": false,
+          "slot": 1
+      },
+      {
+          "ability": {
+              "name": "chlorophyll",
+              "url": "https://pokeapi.co/api/v2/ability/34/"
+          },
+          "is_hidden": true,
+          "slot": 3
+      }
+  ],
+  "base_experience": 64,
+  "forms": [
+      {
+          "name": "bulbasaur",
+          "url": "https://pokeapi.co/api/v2/pokemon-form/1/"
+      }
+  ],
+
+
+
+Request 2 - Pokemon 150, MewTwo: https://pokeapi.co/api/v2/pokemon/150
+
+{
+  "abilities": [
+      {
+          "ability": {
+              "name": "pressure",
+              "url": "https://pokeapi.co/api/v2/ability/46/"
+          },
+          "is_hidden": false,
+          "slot": 1
+      },
+      {
+          "ability": {
+              "name": "unnerve",
+              "url": "https://pokeapi.co/api/v2/ability/127/"
+          },
+          "is_hidden": true,
+          "slot": 3
+      }
+  ],
+  "base_experience": 340,
+  "forms": [
+      {
+          "name": "mewtwo",
+          "url": "https://pokeapi.co/api/v2/pokemon-form/150/"
+      }
+  ],
+
+
+
+Request 3 - Mew: https://pokeapi.co/api/v2/pokemon/mew
+
+{
+  "abilities": [
+      {
+          "ability": {
+              "name": "synchronize",
+              "url": "https://pokeapi.co/api/v2/ability/28/"
+          },
+          "is_hidden": false,
+          "slot": 1
+      }
+  ],
+  "base_experience": 300,
+  "forms": [
+      {
+          "name": "mew",
+          "url": "https://pokeapi.co/api/v2/pokemon-form/151/"
+      }
+  ],
+
+
+
+Request 4 - Pokemon 33, Nidorino: https://pokeapi.co/api/v2/pokemon/33
+
+{
+  "abilities": [
+      {
+          "ability": {
+              "name": "poison-point",
+              "url": "https://pokeapi.co/api/v2/ability/38/"
+          },
+          "is_hidden": false,
+          "slot": 1
+      },
+      {
+          "ability": {
+              "name": "rivalry",
+              "url": "https://pokeapi.co/api/v2/ability/79/"
+          },
+          "is_hidden": false,
+          "slot": 2
+      },
+      {
+          "ability": {
+              "name": "hustle",
+              "url": "https://pokeapi.co/api/v2/ability/55/"
+          },
+          "is_hidden": true,
+          "slot": 3
+      }
+  ],
+  "base_experience": 128,
+  "forms": [
+      {
+          "name": "nidorino",
+          "url": "https://pokeapi.co/api/v2/pokemon-form/33/"
+      }
+  ],
+
+  
+
+  Request 5 - Pokemon 107, Hitmonchan: https://pokeapi.co/api/v2/pokemon/107
+
+  {
+    "abilities": [
+        {
+            "ability": {
+                "name": "keen-eye",
+                "url": "https://pokeapi.co/api/v2/ability/51/"
+            },
+            "is_hidden": false,
+            "slot": 1
+        },
+        {
+            "ability": {
+                "name": "iron-fist",
+                "url": "https://pokeapi.co/api/v2/ability/89/"
+            },
+            "is_hidden": false,
+            "slot": 2
+        },
+        {
+            "ability": {
+                "name": "inner-focus",
+                "url": "https://pokeapi.co/api/v2/ability/39/"
+            },
+            "is_hidden": true,
+            "slot": 3
+        }
+    ],
+    "base_experience": 159,
+    "forms": [
+        {
+            "name": "hitmonchan",
+            "url": "https://pokeapi.co/api/v2/pokemon-form/107/"
+        }
+    ],
