c85182e88285888469a898e4917dfb6c1e7d3dd8
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2022-08-03 23:45:14 -0500

    prettier code update

--- routing-practice/src/App.js
+++ routing-practice/src/App.js
@@ -1,22 +1,19 @@
-import './App.css';
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Home from './components/Home'
-import URLText from './components/URLText';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import URLText from "./components/URLText";
 
 function App() {
-
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path='/home' element={<Home />} />
-          <Route path='/:text' element={<URLText />} />
-          <Route path='/:text/:color/:bgColor' element={<URLText />} />
-        </Routes>
-      </BrowserRouter>
-      <input type='checkbox' id='darkmode-toggle' />
-      <label htmlFor='darkmode-toggle'></label>
-      <div className='backgroundChange'></div>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/:text" element={<URLText />} />
+        <Route path="/:text/:color/:bgColor" element={<URLText />} />
+      </Routes>
+      <input type="checkbox" id="darkmode-toggle" />
+      <label htmlFor="darkmode-toggle"></label>
+      <div className="backgroundChange"></div>
     </div>
   );
 }
